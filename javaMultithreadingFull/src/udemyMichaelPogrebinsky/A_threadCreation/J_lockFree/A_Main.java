0100007329499ff644740d2cd3cbb79d8259c679
Sayak Chowdhury <sayak.chowdhury@mavenir.com>
2025-10-24 20:04:45 +0530

    lockfree ongoing

---
+++ javaMultithreadingFull/src/udemyMichaelPogrebinsky/A_threadCreation/J_lockFree/A_Main.java
@@ -0,0 +1,132 @@
+package udemyMichaelPogrebinsky.A_threadCreation.J_lockFree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.LockSupport;
+
+public class A_Main {
+    public static void main(String[] args) throws InterruptedException {
+        String oldName = "old name";
+        String newName = "new name";
+
+        AtomicReference<String> atomicReference = new AtomicReference<>(oldName);
+        if(atomicReference.compareAndSet(oldName,newName)){
+            System.out.println("New value is: "+atomicReference.get());
+        }else{
+            System.out.println("Nothing happened.");
+        }
+
+        StandardStack<Integer> stack = new StandardStack<>();
+        Random random = new Random();
+
+        for(int i=0;i<100000;i++){
+            stack.push(random.nextInt());
+        }
+
+        List<Thread> threads = new ArrayList<>();
+        int pushingThreads = 2;
+        int poppingThreads = 2;
+
+        for(int i=0;i<pushingThreads;i++){
+            Thread thread = new Thread(()->{
+                while (true){
+                    stack.push(random.nextInt());
+                }
+            });
+            thread.setDaemon(true);
+            threads.add(thread);
+        }
+
+        for(int i=0;i<poppingThreads;i++){
+            Thread thread = new Thread(()->{
+                while (true){
+                    stack.pop();
+                }
+            });
+            thread.setDaemon(true);
+            threads.add(thread);
+        }
+        for(Thread thread:threads){
+            thread.start();
+        }
+        Thread.sleep(10000);
+        System.out.println(stack.getCounter()+" operations were performed in 10 seconds");
+    }
+
+    public static class LockFreeStack<T>{
+        private AtomicReference<StackNode<T>> head = new AtomicReference<>();
+        private AtomicInteger counter = new AtomicInteger(0);
+
+        public void push(T value){
+            StackNode<T> newHeadNode = new StackNode<>(value);
+            while(true){
+                StackNode<T> currentHeadNode = head.get();
+                newHeadNode.next = currentHeadNode;
+                if(head.compareAndSet(currentHeadNode,newHeadNode)){
+                    break;
+                }else{
+                    LockSupport.parkNanos(1);
+                }
+            }
+            counter.incrementAndGet();
+        }
+
+        public T pop(){
+            StackNode<T> currentHeadNode = head.get();
+            StackNode<T> newHeadNode;
+
+            while(currentHeadNode!=null){
+                newHeadNode = currentHeadNode.next;
+                if(head.compareAndSet(newHeadNode,currentHeadNode)){
+                    break;
+                }else{
+                    LockSupport.parkNanos(1);
+                    currentHeadNode = head.get();
+                }
+            }
+            counter.incrementAndGet();
+            return currentHeadNode!=null? currentHeadNode.value : null;
+        }
+
+        public int getCounter(){
+            return counter.get();
+        }
+    }
+
+    private static class StackNode<T>{
+        public T value;
+        public StackNode<T> next;
+
+        public StackNode(T value){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static class StandardStack<T>{
+        private StackNode<T> head;
+        private int counter = 0;
+
+        public synchronized void push(T val){
+            StackNode<T> newHead = new StackNode<>(val);
+            newHead.next = head;
+            head = newHead;
+            counter++;
+        }
+
+        public synchronized T pop(){
+            if(head==null){
+                counter++;
+                return null;
+            }
+            T value = head.value;
+            head = head.next;
+            counter++;
+            return value;
+        }
+        public int getCounter(){ return counter;}
+    }
+}
