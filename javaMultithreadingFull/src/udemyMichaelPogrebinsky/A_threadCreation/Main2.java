8a0c5e0625427fd780f8cf9b7208bf5a0199efa9
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2025-10-26 12:45:15 +0530

    code for udemy course added

---
+++ javaMultithreadingFull/src/udemyMichaelPogrebinsky/A_threadCreation/Main2.java
@@ -0,0 +1,16 @@
+package udemyMichaelPogrebinsky.A_threadCreation;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+        thread.start();
+    }
+
+    private static class NewThread extends Thread{
+
+        @Override
+        public void run(){
+            System.out.println("Hello from Thread: "+this.getName());
+        }
+    }
+}
