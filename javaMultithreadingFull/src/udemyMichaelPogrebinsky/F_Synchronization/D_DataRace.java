a1bcc16fb51ff6243802b26f570b8c6cbea5e415
Sayak Chowdhury <sayak.chowdhury@mavenir.com>
2025-10-27 15:58:39 +0530

    addition of DataRace code in synchronization

---
+++ javaMultithreadingFull/src/udemyMichaelPogrebinsky/F_Synchronization/D_DataRace.java
@@ -0,0 +1,37 @@
+package udemyMichaelPogrebinsky.F_Synchronization;
+
+public class D_DataRace {
+    public static void main(String[] args) {
+        SharedClass sharedClass = new SharedClass();
+        Thread t1 = new Thread(()->{
+            for(int i=0;i<Integer.MAX_VALUE;i++){
+                sharedClass.increment();
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            for(int i=0;i<Integer.MAX_VALUE;i++){
+                sharedClass.checkForDataRace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    public static class SharedClass{
+        private int x=0;
+        private int y=0;
+
+        public void increment(){
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace(){
+            if(y>x){
+                System.out.println("y > x -> Data race happened...");
+            }
+        }
+    }
+}
